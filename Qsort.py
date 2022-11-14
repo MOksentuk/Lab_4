d22572cd44b58adb98dcc32a09b217761b454d4f
MOksentuk <113674979+MOksentuk@users.noreply.github.com>
2022-11-14 21:27:03 +0300

    completed qsort

--- Qsort.py
+++ Qsort.py
@@ -1,27 +1,16 @@
-l = [3, 2, 4, 1, 6, 8, 5, 8]
-l1 = [5, 4, 6]
-
-
-def step(l):
-    m = l[0]
-    SmallerThanm = []
-    BiggerThanm = []
-    for i in range(1, len(l)):
-        if l[i] < m:
-            SmallerThanm.append(l[i])
-        elif l[i] >= m:
-            BiggerThanm.append(l[i])
-    SmallerThanm.append(m)
-    if len(SmallerThanm) <= 2 and len(BiggerThanm) <= 1:
-        res = SmallerThanm + BiggerThanm
-        return SmallerThanm, BiggerThanm
+def Qsort(mass):
+    if len(mass) <= 1:
+        return mass
     else:
-        step(SmallerThanm)
-        step(BiggerThanm)
-
-
-def qsort(l):
-    a,b=step(l)
-
-
-print(step(l1))
+        pillar = mass[0]
+        SmallerThanPillar = []
+        EqualPillar = []
+        BiggerThanPillar = []
+        for el in mass:
+            if el < pillar:
+                SmallerThanPillar.append(el)
+            elif el > pillar:
+                BiggerThanPillar.append(el)
+            else:
+                EqualPillar.append(el)
+        return Qsort(SmallerThanPillar) + EqualPillar + Qsort(BiggerThanPillar)
